cd15dec5aec118a7a78955ba5c09dede0b2abfa5
Shweepsi <shweepsi@gmail.com>
2025-07-19 08:31:06 +0200

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,21 +1,7 @@
-import { CapacitorConfig } from '@capacitor/cli'
-
-const config: CapacitorConfig = {
-  appId: 'app.lovable.idlegrow',
+const config = {
+  appId: 'com.shweepsi.idlegrow',
   appName: 'Idle Grow',
   webDir: 'dist',
-  bundledWebRuntime: false,
-  server: {
-    url: 'https://28164eb9-0f8a-43bd-9b5c-dc8227ba1150.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#22c55e',
-      showSpinner: false
-    }
-  }
-}
+};
 
-export default config
+export default config;
